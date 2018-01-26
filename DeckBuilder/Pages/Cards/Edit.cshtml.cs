09be5028745a4b9cca8c5e9a73e9be014bfeb727
Adam Davenport <cabewz@gmail.com>
2018-01-25 20:49:39 -0500

    Cleaning up using statements

---
+++ DeckBuilder/Pages/Cards/Edit.cshtml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DeckBuilder.Models;
+
+namespace DeckBuilder.Pages.Cards
+{
+    public class EditModel : PageModel
+    {
+        private readonly DeckBuilder.Models.DeckBuilderContext _context;
+
+        public EditModel(DeckBuilder.Models.DeckBuilderContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Card Card { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Card = await _context.Card.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (Card == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Card).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CardExists(Card.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CardExists(string id)
+        {
+            return _context.Card.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ DeckBuilder/Pages/Cards/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DeckBuilder.Models;
+
+namespace DeckBuilder.Pages.Cards
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DeckBuilder.Models.DeckBuilderContext _context;
+
+        public IndexModel(DeckBuilder.Models.DeckBuilderContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Card> Card { get;set; }
+
+        public async Task OnGetAsync(string SortOrder, string SearchString)
+        {
+			IQueryable<Card> CardIQ = from c in _context.Card select c;
+
+			if(!String.IsNullOrEmpty(SearchString))
+			{
+				CardIQ = CardIQ.Where(c => c.Name.Contains(SearchString));
+			}
+			Card = await CardIQ.AsNoTracking().ToListAsync();
+        }
+    }
+}
