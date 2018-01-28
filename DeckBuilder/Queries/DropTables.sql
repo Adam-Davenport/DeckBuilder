61411427862d2578285989149ad896a7df075eb7
Adam Davenport <cabewz@gmail.com>
2018-01-28 12:16:02 -0500

    Creating query to drop tables for testing purposes

---
+++ DeckBuilder/Queries/DropTables.sql
@@ -0,0 +1,10 @@
+﻿Begin Transaction
+drop table __EFMigrationsHistory;
+drop table Boosters;
+drop table CardColors;
+drop table Cards;
+drop table DeckComments;
+drop table DeckLists;
+drop table Decks;
+drop table [Sets];
+Commit;
