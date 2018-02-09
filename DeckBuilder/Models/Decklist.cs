b1a8e7bf7fb7fb8bfe7c4166318156a65e8939ad
Adam Davenport <cabewz@gmail.com>
2018-02-08 21:03:26 -0500

    Formatting

--- DeckBuilder/Models/Decklist.cs
+++ DeckBuilder/Models/Decklist.cs
@@ -6,11 +6,10 @@ namespace DeckBuilder.Models
 	public class Decklist
 	{
 		public int DeckId { get; set; }
-		public string CardId { get; set; }
-		public int Count { get; set; }
 		public Deck Deck { get; set; }
+		public string CardId { get; set; }
 		public Card Card { get; set; }
-
+		public int Count { get; set; }
 
 		public Decklist()
 		{ }
