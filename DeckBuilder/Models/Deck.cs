4475f95f504e7522f925cb7620edc9fe14ecb81b
Adam Davenport <cabewz@gmail.com>
2018-02-08 21:03:13 -0500

    Adding image url for Decks

--- DeckBuilder/Models/Deck.cs
+++ DeckBuilder/Models/Deck.cs
@@ -7,6 +7,7 @@ namespace DeckBuilder.Models
     {
         public int Id { get; set; }
 		public string Name { get; set; }
+		public string ImageUrl { get; set; }
 		public string Description { get; set; }
         public string Author { get; set; }
         public DateTime Date { get; private set; }
